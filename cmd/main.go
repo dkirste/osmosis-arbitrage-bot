ca8fcf29571f7806b9eb2c6661e476dea1d3793f
Daniel Kirste <danielkirste@icloud.com>
2022-05-05 14:47:03 +0200

    Added mywallet

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,49 @@ package main
 
 import (
 	"github.com/dkirste/arbbot/arbbot"
+	"github.com/dkirste/arbbot/mywallet"
 )
 
 func main() {
-	grpcNodes := []string{"46.38.245.118:9090", "osmosis.strange.love:9090", "37.120.165.37:9090"}
-	//grpcNodes = []string{"osmosis.strange.love:9090"}
-	rpcNodes := []string{"tcp://46.38.245.118:26657"}
+	grpcNodes := []string{"46.38.245.118:9090",
+		"osmosis.strange.love:9090",
+		"37.120.165.37:9090",
+		"34.68.29.156:9090",
+		"135.148.55.229:9090",
+		"107.178.208.25:9090",
+		"37.120.165.37:9090",
+		//"65.108.127.166:9090", sometimes down
+		//"65.108.101.108:9090", sometimes down
+		//"65.21.198.130:9090", maybe atom
+		//"45.34.1.114:9090", atom node
+		"35.222.218.55:9090",
+	}
+	//grpcNodes = []string{"37.120.165.37:9090"}
+	rpcNodes := []string{"tcp://46.38.245.118:26657",
+		"tcp://37.120.165.37:26657",
+		"https://rpc-osmosis.ecostake.com:443",
+		//"https://osmosis.validator.network:443", // banning mempool scan
+		"http://34.68.29.156:26657",
+		"http://135.148.55.229:26657",
+		"http://107.178.208.25:26657",
+		"http://37.120.165.37:26657",
+		"http://65.108.127.166:26657",
+		"http://65.108.101.108:26657",
+		"http://65.21.198.130:36657",
+		//"http://45.34.1.114:26657",
+		"http://35.222.218.55:26657",
+	}
+	//rpcNodes = []string{"tcp://46.38.245.118:26657"}
 	infoMachineBaseUrl := "https://api-osmosis.imperator.co"
 
-	address := "osmo12xjjkmefue4l655h32nxf7ducnvn4tndwqzf85"
-	privateKeyArmor := "-----BEGIN TENDERMINT PRIVATE KEY-----\nkdf: bcrypt\nsalt: C0F7FAA463DFEC95EC78990480CD23CC\ntype: secp256k1\n\nMDngiOXNmCdHIrFtEdfMkvKM9mlmjhLDIeOFhZPltTw9ogjTnrQin8bcS8BJ48G2\nRLeU9FnEnzSijxk8Zg8JWvFA0TLSsqjR7Mhio7s=\n=FKgG\n-----END TENDERMINT PRIVATE KEY-----"
-	privateKeyPassphrase := "aoisdhgoiuahjgklasdjkfahsdfaosdfp"
+	address, privateKeyArmor, privateKeyPassphrase := mywallet.GetPrivateKey("test")
 
-	var liquidityThreshold float64 = 1000
+	var liquidityThreshold float64 = 10
 
 	bot := arbbot.ArbBot{}
 	bot.Setup(grpcNodes, rpcNodes, infoMachineBaseUrl, address, privateKeyArmor, privateKeyPassphrase, liquidityThreshold)
-	bot.Run(6)
+	bot.RunBlockArb(6)
+	//bot.RunMempoolArb(4, 1000)
 
 	return
 }
